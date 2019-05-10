0a87918753abffad89f8caeb697d14addae8683d
Hidalgo <hidalgochpnce@gmail.com>
2019-05-10 10:52:19 -0500

    Se creo la base de la aplicacion

--- es/mtc-admin/models/conexion.php
+++ es/mtc-admin/models/conexion.php
@@ -1,9 +1,9 @@
 <?php
 function conectarse(){
     $servidor="localhost";
-    $usuario="pandanin_mount_e";
+    $usuario="b2u4b3e3_mou_es";
     $password="Hidalgo123@";
-    $bd="pandanin_mountain_es";
+    $bd="b2u4b3e3_mountain_es";
 
     $conectar = new mysqli($servidor,$usuario,$password,$bd) or die("No se pudo conectar a la bd");
     return $conectar;
--- mtc-admin/models/conexion.php
+++ mtc-admin/models/conexion.php
@@ -1,9 +1,9 @@
 <?php
 function conectarse(){
     $servidor="localhost";
-    $usuario="pandanin_mount";
+    $usuario="b2u4b3e3_mou_us";
     $password="Hidalgo123@";
-    $bd="pandanin_mountain";
+    $bd="b2u4b3e3_mountain_us";
 
     $conectar = new mysqli($servidor,$usuario,$password,$bd) or die("No se pudo conectar a la bd");
     return $conectar;
